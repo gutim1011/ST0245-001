70f7b30c1ae1c8bc3e9be5f61f66fda544f218be
gutim1011 <104235522+gutim1011@users.noreply.github.com>
2022-08-14 11:07:12 -0500

    Update code.py

--- code.py
+++ code.py
@@ -10,24 +10,5 @@ import pandas as pd
 # In[7]:
 
 
-calles = pd.read_csv('calles_de_medellin_con_acoso.csv',sep=';')
-poligono = pd.read_csv('poligono_de_medellin.csv',sep=';')
-
-
-# In[6]:
-
-
-calles
-
-
-# In[8]:
-
-
-poligono
-
-
-# In[ ]:
-
-
-
-
+pd.read_csv('calles_de_medellin_con_acoso.csv',sep=';')
+pd.read_csv('poligono_de_medellin.csv',sep=';')
