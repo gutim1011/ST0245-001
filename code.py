a2ea5b07c0255af177f2b5f2a69152b77fe37042
gutim1011 <104235522+gutim1011@users.noreply.github.com>
2022-08-14 11:06:18 -0500

    Add files via upload

---
+++ code.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[7]:
+
+
+calles = pd.read_csv('calles_de_medellin_con_acoso.csv',sep=';')
+poligono = pd.read_csv('poligono_de_medellin.csv',sep=';')
+
+
+# In[6]:
+
+
+calles
+
+
+# In[8]:
+
+
+poligono
+
+
+# In[ ]:
+
+
+
+
